342048706e10f661f309094bd4a982aa4950438c
edgjj <tstd@tstd.tech>
2022-09-05 23:05:47 +0500

    Value: Calculation works. There are some issues related to declare order, breaking calculations.

--- include/cppgrad/value.hpp
+++ include/cppgrad/value.hpp
@@ -8,6 +8,7 @@
 #include <set>
 #include <iostream>
 #include <memory>
+#include <optional>
 
 namespace cppgrad 
 {
@@ -17,14 +18,23 @@ template <typename T>
 class Value;
 
 template <typename T>
-using ValuePtr = Value<T>*;
+using ValuePtr = std::shared_ptr<Value<T>>;
 
 namespace util
 {
 
 template <typename T>
+struct ValueComparator
+{
+	constexpr bool operator()(const ValuePtr<T>& a, const ValuePtr<T>& b) const
+	{
+		return a->_storage < b->_storage;
+	}
+};
+
+template <typename T>
 void build_topo(ValuePtr<T> v
-	, std::set<ValuePtr<T>>& visited
+	, std::set<ValuePtr<T>, util::ValueComparator<T>>& visited
 	, std::vector<ValuePtr<T>>& topo)
 {
 	if (!v)
@@ -34,7 +44,8 @@ void build_topo(ValuePtr<T> v
 
 	if (visited.count(v) == 0)
 	{
-		std::cout << "[toposort] visiting: " << v << " op type: " << v->_op << '\n';
+		std::cout << "[toposort] visiting: " << v << " op type: " << v->_op << " grad: " << v->_storage->grad << " value: " << v->_storage->val << '\n';
+		std::cout << "\t" << " parent 1: " << v->_prev.first << " parent 2: " << v->_prev.second << '\n';
 
 		visited.insert(v);
 
@@ -49,24 +60,28 @@ void build_topo(ValuePtr<T> v
 
 }
 
+
 template <typename T>
-class Value : std::enable_shared_from_this<Value<T>>
+class Value
 {
 public:
-	using BackwardFun = std::function<void(T&, T&)>;
+	using BackwardFun = std::function<void()>;
 
 	/*
 		We use this since all ops are binary or unary.
 	*/
-	using BinaryPair = std::pair<Value<T>*, Value<T>*>;
+	using BinaryPair = std::pair<
+		ValuePtr<T>, 
+		ValuePtr<T>>;
+
+	Value()
+		: _storage(std::make_shared<ValueData<T>>(0, 0))
+	{}
 
 	Value(T&& data, 
-		BackwardFun&& backward = BackwardFun{}, 
 		BinaryPair parents = BinaryPair{},
 		std::string op_name = "")
-		: _data{ std::move(data) }
-		, _grad{ 0 }
-		, _backward{ std::move(backward) }
+		: _storage(std::make_shared<ValueData<T>>(std::move(data), 0))
 		, _prev{ parents }
 		, _op{ op_name }
 	{}
@@ -74,39 +89,52 @@ public:
 	// holy shit
 	//using util::build_topo;
 	friend void util::build_topo(ValuePtr<T> v
-		, std::set<ValuePtr<T>>& visited
+		, std::set<ValuePtr<T>, util::ValueComparator<T>>& visited
 		, std::vector<ValuePtr<T>>& topo);
 
-	friend Value<T> operator+(Value<T>& lhs, Value<T>& rhs)
+	// needed for set support
+	friend struct util::ValueComparator<T>;
+
+	Value<T> operator+(Value<T>& rhs)
 	{
-		BackwardFun fun = [=](T&, T& localGrad) {
-			lhs->_grad += localGrad;
-			rhs->_grad += localGrad;
-			std::cout << "[+] lhs grad: " << _lhs->_grad << " rhs grad: " << _rhs->_grad << '\n';
+		auto self = _storage,
+			other = rhs._storage;
+
+		BinaryPair parents{ 
+			std::make_shared<Value<T>>(*this), 
+			std::make_shared<Value<T>>(rhs)
 		};
 
-		BinaryPair parents{ &lhs, &rhs };
-		auto output = Value<T>(lhs._data + rhs._data
-			, std::move(fun)
+		auto output = Value<T>(self->val + other->val
 			, parents
 			, "+");
+
+		output._backward = [self, other, out = output._storage]() {
+			self->grad += out->grad;
+			other->grad += out->grad;
+		};
 
 		return output;
 	}
 	
-	friend Value<T> operator*(Value<T>& lhs, Value<T>& rhs)
+	Value<T> operator*(Value<T>& rhs)
 	{
-		BackwardFun fun = [=](T&, T& localGrad) {
-			lhs._grad += rhs._data * localGrad;
-			rhs._grad += lhs._data * localGrad;
-			std::cout << "[*] lhs grad: " << lhs._grad << " rhs grad: " << rhs._grad << '\n';
+		auto self = _storage,
+			other = rhs._storage;
+
+		BinaryPair parents{ 
+			std::make_shared<Value<T>>(*this), 
+			std::make_shared<Value<T>>(rhs)
 		};
 
-		BinaryPair parents{ &lhs, &rhs };
-		auto output = Value<T>(lhs._data * rhs._data
-			, std::move(fun)
+		auto output = Value<T>(self->val * other->val
 			, parents
 			, "*");
+
+		output._backward = [self, other, out = output._storage]() {
+			self->grad += other->val * out->grad;
+			other->grad += self->val * out->grad;
+		};
 
 		return output;
 	}
@@ -114,33 +142,41 @@ public:
 	Value<T> pow(Value<T>& rhs)
 	{
 		using std::pow;
+
+		auto self = lhs._storage;
 		
-		BackwardFun fun = [=](T&, T& localGrad) {
-			_grad += rhs * pow(_data, rhs - 1) * localGrad;
+		BinaryPair parents{
+			std::make_shared<Value<T>>(*this),
+			std::nullptr_t{}
 		};
 
-		BinaryPair parents{ this, nullptr };
-
-		auto output = Value<T>(pow(_data, rhs)
-			, std::move(fun)
+		auto output = Value<T>(pow(self->val, rhs)
 			, parents
 			, "pow");
+
+		output._backward = [self, rhs, out = output._storage]() {
+			self->grad += rhs * pow(self->val, rhs - 1) * out->grad;
+		};
 
 		return output;
 	}
 
 	Value<T> relu()
 	{
-		BackwardFun fun = [=](T& localData, T& localGrad) {
-			_grad += (localData > 0) * localGrad;
+		auto self = _storage;
+
+		BinaryPair parents{
+			std::make_shared<Value<T>>(*this),
+			std::nullptr_t{}
 		};
 
-		BinaryPair parents{ this, nullptr };
-
-		auto output = Value<T>(_data < 0 ? 0 : _data
-			, std::move(fun)
+		auto output = Value<T>(self->val < 0 ? 0 : self->val // this should be replaced with better op
 			, parents
 			, "relu");
+
+		output._backward = [self, out = output._storage]() {
+			self->grad += (out->val > 0) * out->grad;
+		};
 
 		return output;
 	}
@@ -151,36 +187,54 @@ public:
 	void backward()
 	{
 		std::vector<ValuePtr<T>> topo;
-		std::set<ValuePtr<T>> visited;
+		// we got std::hash specialization for this, so its ok
+		std::set<ValuePtr<T>, util::ValueComparator<T>> visited;
 
-		_grad = T( 1 );
+		auto self = std::make_shared<Value<T>>(*this);
+		util::build_topo(self, visited, topo);
 		
-		util::build_topo(this, visited, topo);
-		
+		_storage->grad = T(1);
+
 		for (auto rit = visited.rbegin(); rit != visited.rend(); rit++)
 		{
 			ValuePtr<T> cur = *rit;
 			// check if _backward exists
 			if (cur->_backward)
 			{
-				cur->_backward(cur->_data, cur->_grad);
+				cur->_backward();
+
+				std::cout << "[bward] visiting: " << cur << " op type: " << cur->_op << " grad: " << cur->_storage->grad << " value: " << cur->_storage->val << '\n';
+
 			}
 		}
 	}
 
 	T& get_grad()
 	{
-		return _grad;
+		return _storage->grad;
 	}
 
 	T& get_data()
 	{
-		return _data;
+		return _storage->val;
 	}
 
 private:
-	T _grad;
-	T _data;
+
+	template <typename T>
+	struct ValueData
+	{
+		ValueData(T&& _val, T&& _grad)
+			: val(std::move(_val))
+			, grad(std::move(_grad))
+		{}
+
+		T val;
+		T grad;
+	};
+
+	using DataPtr = std::shared_ptr<ValueData<T>>;
+	DataPtr _storage;
 
 	BackwardFun _backward; // already nulled
 	BinaryPair _prev; 
--- tests/cppgrad_tests.cpp
+++ tests/cppgrad_tests.cpp
@@ -5,16 +5,18 @@ using namespace cppgrad;
 
 TEST(SanityTest, MicrogradInheritedSuite)
 {
-    Value x = -2.0;
-    auto z = Value{ 2.0 } * x + Value{ 2.0 } + x;
-    auto q = z.relu() + z * x;
-    auto h = (z * z).relu();
-    h.backward();
-    //auto y = h + q + q * x;
-    //y.backward();
+    auto x = Value(-2.0);
+    auto z = Value(2.0) * x + Value(2.0) + x;
 
-    //std::cout << "current x grad value: " << x.get_grad() << '\n';
-    //std::cout << "current y value: " << y.get_data() << '\n';
+    auto h = (z * z).relu();
+    auto q = z.relu() + z * x;
+
+    auto y = h + q + q * x;
+   // auto y = h + q;
+    y.backward();
+
+    std::cout << "current x grad value: " << x.get_grad() << '\n';
+    std::cout << "current y value: " << y.get_data() << '\n';
 }
 
 //TEST(MoreOpsTest, MicrogradInheritedSuite)
