9c8c346bacb069065cf6edcf0d00eaa73707b08a
edgjj <tstd@tstd.tech>
2022-12-23 15:00:03 +0500

    Itertools: Remove tuple_of.

--- include/cppgrad/itertools/combine.hpp
+++ include/cppgrad/itertools/combine.hpp
@@ -7,22 +7,6 @@
 
 namespace cppgrad::itertools {
 
-namespace impl {
-    template <size_t I, typename T>
-    struct tuple_n {
-        template <typename... Args>
-        using type = typename tuple_n<I - 1, T>::template type<T, Args...>;
-    };
-
-    template <typename T>
-    struct tuple_n<0, T> {
-        template <typename... Args>
-        using type = std::tuple<Args...>;
-    };
-    template <size_t I, typename T>
-    using tuple_of = typename tuple_n<I, T>::template type<>;
-}
-
 template <typename... Iterators>
 struct combined_iterator {
     using IterTuple = std::tuple<Iterators...>;
